0ff3fbefa53d79591fca7862dd40bcd204d8f0a1
Jérémie Soria <jsoria@sat.qc.ca>
2017-04-18 11:31:45 -0400

    i3 config

---
+++ i3/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+
+sudo apt install i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
+    qt4-qtconfig sysstat acpi git automake build-essential gtk-doc-tools \
+    gobject-introspection xbacklight scrot kbdd xautolock pavucontrol xscreensaver \
+    lxappearance dmenu i3-dmenu-desktop
+
+# Install playerctl
+cd /tmp && rm -rf playerctl
+git clone https://github.com/acrisci/playerctl.git
+cd playerctl
+./autogen.sh
+make && sudo make install
+
+# Install xkblayout-state
+cd /tmp && rm -rf xkblayout-state
+git clone https://github.com/nonpop/xkblayout-state.git
+cd xkblayout-state
+make -j$(nproc)
+cp xkblayout-state ${source_directory}/
